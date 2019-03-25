45320f9d0acf037c57cc5c4f8cf07762e36c03e1
VIBHOR94 <vibhorgupta2008@aol.in>
2019-03-26 02:01:51 +0530

    Add route to dislike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -115,4 +115,39 @@ router.post(
   }
 );
 
+//@route   POST api/posts/unlike/:id
+//@desc    Unlike post
+//@access  Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(() => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notliked: "You have not liked this post yet" });
+          }
+
+          // Get the remove index
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // Splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.json(err));
+    });
+  }
+);
+
 module.exports = router;
